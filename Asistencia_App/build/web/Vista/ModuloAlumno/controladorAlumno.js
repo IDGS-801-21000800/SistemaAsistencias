45c516c7db7d11972ba56c3c10103175cb73a894
Juarezalvizo <108360946+Juarezalvizo@users.noreply.github.com>
2023-12-11 22:18:09 -0600

    Add files via upload
    Se acaba de reemplazar la carpeta de vista por esta nueva donde ya esta esto completo, solo es la carpeta de vistas en el de asistencia_app, web , vista

--- Asistencia_App/build/web/Vista/ModuloAlumno/controladorAlumno.js
+++ Asistencia_App/build/web/Vista/ModuloAlumno/controladorAlumno.js
@@ -1,6 +1,12 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
+// Función para mostrar o ocultar el card de asistencia
+  function toggleAsistenciaCard() {
+    const asistenciaCard = document.getElementById('asistenciaCard');
+    asistenciaCard.classList.toggle('d-none');
+  }
 
-
+  // Función para mostrar información en el card de asistencia
+  function mostrarInformacion(dia, profesor) {
+    // Aquí puedes realizar acciones específicas cuando se hace clic en una celda
+    // Por ahora, simplemente mostraremos u ocultaremos el card de asistencia
+    toggleAsistenciaCard();
+  }
